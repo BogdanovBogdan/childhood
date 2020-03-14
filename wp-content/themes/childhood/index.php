49815f4328a8874aef1871c979a8b9f5c8533171
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-03-14 18:50:37 +0300

    added custom fields for "aboutus" section

--- wp-content/themes/childhood/index.php
+++ wp-content/themes/childhood/index.php
@@ -93,10 +93,10 @@
         </div>
         <div class="specialists" id="specialists">
             <div class="container">
-                <div class="title">Наша команда</div>
+                <div class="title"><?php the_field("team_title") ?></div>
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1">
-                        <img class="specialists__img" src="<?php echo bloginfo("template_url"); ?>/assets/img/team.jpg" alt="наша команда">
+                        <img class="specialists__img" src="<?php echo get_field("team_img"); ?>" alt="наша команда">
                     </div>
                 </div>
             </div>
@@ -191,9 +191,11 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1">
-                        <div class="toys__alert">
-                            <span>Не нашли то, что искали?</span> Свяжитесь с нами - и мы с радостью создадим любую игрушку по вашему желанию. Вы можете выбрать все: размер, материал, формы...!
-                        </div>
+                            <?php 
+                                if ( get_field( "checkbox_not-found", 2 ) ): ?>
+                                    <div class="toys__alert"> <?php  the_field( "text_not-found", 2 ); ?> </div>
+                                <?php endif;                      
+                            ?> 
                     </div>
                 </div>
             </div>
@@ -201,35 +203,51 @@
 
         <div class="aboutus" id="aboutus">
             <div class="container">
-                <h1 class="title">Наша история</h1>
+                <h1 class="title"><?php the_field("aboutus_title") ?></h1>
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="subtitle">
-                            Все начиналось с желания
+                            <?php the_field("aboutus_subtitle1") ?>
                         </div>
                         <div class="aboutus__text">
-                            Желания сделать как можно больше детей счастливыми. Именно с этой идеи все и зарождалось.
-                            <br><br>
-                            Первые игрушки, сделанные вручную были классическими плюшевыми медведями, которые разошлись настолько быстро, что нас завалили заказами на несколько месяцев вперед. Именно в то время мы поняли, что идем правильным путем, вкладывая все силы и эмоции в наши игрушки.
+                            <?php the_field("aboutus_text1") ?>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <img class="aboutus__img" src="<?php echo bloginfo("template_url"); ?>/assets/img/about_1.jpg" alt="мир детства">
+                        <?php
+                            $img = get_field("aboutus_img1");
+
+                            if ( !empty($img) ): ?>
+                                <img class="aboutus__img"
+                                src="<?php echo $img["url"]; ?>" 
+                                alt="<?php echo $img["alt"]; ?>">
+                            <?php endif;
+                            
+                        ?>
+                    
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-6">
-                        <img class="aboutus__img" src="<?php echo bloginfo("template_url"); ?>/assets/img/about_2.jpg" alt="мир детства">
+                        <?php
+                            $img = get_field("aboutus_img2");
+
+                            if (!empty($img)): ?>
+                                <img class="aboutus__img"
+                                src="<?php echo $img["url"] ?>" 
+                                alt="<?php echo $img["alt"] ?>">
+                            <?php endif;
+                            
+                        ?>
+                    
                     </div>
                     <div class="col-lg-6">
                         <div class="subtitle">
-                            Главное - качество
+                            <?php the_field("aboutus_subtitle2") ?>
                         </div>
                         <div class="aboutus__text">
-                            Мы делали их вручную, из лучших материалов и не жалея времени. Но мы росли и наш ассортимент расширился и фабричными изделиями.
-                            <br><br>
-                            Выбирая нас, вы можете быть уверены, что мы всегда следим за качеством закупок и никогда не предоставим вам опасный или некачественный товар.
+                            <?php the_field("aboutus_text2") ?>
                         </div>
                     </div>
                 </div>
@@ -237,16 +255,24 @@
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="subtitle">
-                            Современные технологии
+                            <?php the_field("aboutus_subtitle3") ?>
                         </div>
                         <div class="aboutus__text">
-                            И даже спустя столько лет мы продолжаем создавать игрушки вручную. Как самые простые, так и те, что идут в ногу со временем. Добавляя электроники и оживляя лучших друзей ребятишек, мы всегда следим за качеством и безопасностью. Каждая отдельная игрушка проходит индивидуальный контроль по всем необходимым стандартам.
-                            <br><br>
-                            Ведь счастливое лицо ребенка - это лучшая награда для нас!
+                            <?php the_field("aboutus_text3") ?>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <img class="aboutus__img" src="<?php echo bloginfo("template_url"); ?>/assets/img/about_3.jpg" alt="мир детства">
+                        <?php
+                            $img = get_field("aboutus_img3");
+
+                            if (!empty($img)): ?>
+                                <img class="aboutus__img"
+                                src="<?php echo $img["url"] ?>" 
+                                alt="<?php echo $img["alt"] ?>">
+                            <?php endif;
+                            
+                        ?>
+                    
                     </div>
                 </div>
             </div>
